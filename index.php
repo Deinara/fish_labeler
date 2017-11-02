3e4e49373aea728a036cfca001e84ac4fcc04da3
Christiaan Blom <g.c.blom14@gmail.com>
2017-11-02 23:59:02 +0100

    style changes

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
-
 <html>
+
 <head>
-<link rel="stylesheet" type="text/css" href="stylesheet_labels.css">
-<link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">
-<script src="https://use.fontawesome.com/d1c050bcc4.js"></script>
+    <link rel="stylesheet" type="text/css" href="stylesheet_labels.css">
+    <link href="https://fonts.googleapis.com/css?family=Palanquin" rel="stylesheet">
+    <script src="https://use.fontawesome.com/d1c050bcc4.js"></script>
 </head>
+
 <body>
 
 <?php
 
-require_once('labelClasses.php');
+    require_once('labelClasses.php');
 
-$FishLabel = new FishLabel;
+    $FishLabel = new FishLabel;
 
-echo $FishLabel->createAllCards();
+    echo $FishLabel->createAllCards();
 
 ?>
 
 </body>
+
 </html>
